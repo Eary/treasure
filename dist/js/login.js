977cb70a6df048f19b0bcc76d7692061e73f0841
heroyy <yy.liu@foxmail.com>
2016-01-20 17:06:24 +0800

    add pages

--- dist/js/login.js
+++ dist/js/login.js
@@ -1,10 +1,8 @@
 $(function(){
     'use strict';
     $(document).on('pageInit','#register',function(e, id, page){
-        alert(id);
     })
     .on('pageInit','#login',function(e, id, page){
-        alert(id);
     });
     
     $.init();
